c71a2c24e8e55505780de9a17539c2df5d6b0740
khaphan2806 <khaphan2806@gmail.com>
2021-09-13 21:29:34 +0700

    add user chat window

--- client/src/views/Chat.vue
+++ client/src/views/Chat.vue
@@ -1,104 +1,116 @@
 <template>
 	<div class="about">
 		<h1>Welcome to ChatPage</h1>
-		<!-- <div class="container" v-for="user in users" :key="user.userID">
-			<div class="status"><status-icon :connected="user.connected" />{{ user.username }}</div>
-		</div> -->
+		<div v-if="!isChat" class="chat-btn">
+			<el-button type="primary" @click="isChat = !isChat">Chat to admin</el-button>
+		</div>
+		<div v-else>
+			<div class="chat-container">
+				<el-scrollbar class="chat-list">
+					<div
+						class="chat-block"
+						:class="{ user: chat.type === 'user' }"
+						v-for="(chat, index) in chatArr"
+						:key="index"
+					>
+						<span>{{ chat.msg }}</span>
+					</div>
+				</el-scrollbar>
+				<div class="chat-controls">
+					<el-form @submit.prevent.native="handleChat">
+						<el-form-item>
+							<el-input v-model="msg" placeholder="Enter your message....">
+								<template #append>
+									<el-button icon="el-icon-s-promotion" @click="handleChat"></el-button>
+								</template>
+							</el-input>
+						</el-form-item>
+					</el-form>
+				</div>
+			</div>
+		</div>
 	</div>
 </template>
 
 <script>
 	import Vue from 'vue';
-	// import StatusIcon from './StatusIcon.vue';
-	import socket from '../socket/socket';
 	export default Vue.extend({
 		name: 'Chat',
 		components: {},
 		data() {
 			return {
-				users: [],
+				isChat: false,
+				msg: '',
+				chatArr: [],
 			};
 		},
-		created() {
-			socket.on('connect', () => {
-				this.users.forEach((user) => {
-					console.log(user);
-					if (user.self) {
-						user.connected = true;
-					}
-				});
-			});
-
-			socket.on('disconnect', () => {
-				this.users.forEach((user) => {
-					if (user.self) {
-						user.connected = false;
-					}
-				});
-			});
-
-			const initReactiveProperties = (user) => {
-				user.connected = true;
-			};
-
-			socket.on('users', (users) => {
-				users.forEach((user) => {
-					user.self = user.userID === socket.id;
-					initReactiveProperties(user);
-				});
-				// put the current user first, and sort by username
-				this.users = users.sort((a, b) => {
-					if (a.self) return -1;
-					if (b.self) return 1;
-					if (a.username < b.username) return -1;
-					return a.username > b.username ? 1 : 0;
-				});
-			});
-
-			socket.on('user connected', (user) => {
-				initReactiveProperties(user);
-				this.users.push(user);
-			});
-
-			socket.on('user disconnected', (id) => {
-				for (let i = 0; i < this.users.length; i++) {
-					const user = this.users[i];
-					if (user.userID === id) {
-						user.connected = false;
-						break;
-					}
+		methods: {
+			handleChat() {
+				if (this.msg.length > 0) {
+					this.chatArr.push({
+						msg: this.msg,
+						type: 'user',
+					});
+					this.msg = '';
 				}
-			});
-
-			// socket.on('private message', ({ content, from }) => {
-			//   for (let i = 0; i < this.users.length; i++) {
-			//     const user = this.users[i];
-			//     if (user.userID === from) {
-			//       user.messages.push({
-			//         content,
-			//         fromSelf: false,
-			//       });
-			//       if (user !== this.selectedUser) {
-			//         user.hasNewMessages = true;
-			//       }
-			//       break;
-			//     }
-			//   }
-			// });
-		},
-		destroyed() {
-			socket.off('connect');
-			socket.off('disconnect');
-			socket.off('users');
-			socket.off('user connected');
-			socket.off('user disconnected');
-			// socket.off('private message');
+			},
 		},
 	});
 </script>
 
-<style scoped>
+<style>
 	.about h1 {
 		text-align: center;
+	}
+
+	.chat-btn {
+		text-align: center;
+	}
+	.chat-container {
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+
+		width: 400px;
+		margin: 0 auto;
+		padding: 20px;
+		height: 600px;
+
+		border: 1px solid #ccc;
+		border-radius: 5px;
+		box-shadow: 0 0 5px #ccc;
+	}
+
+	.chat-list.chat-list {
+		height: 95%;
+	}
+	.el-scrollbar__wrap {
+		overflow: auto !important;
+	}
+
+	.chat-controls {
+		height: 5%;
+	}
+
+	.chat-block {
+		margin-bottom: 15px;
+	}
+
+	.chat-block span {
+		display: inline-block;
+		padding: 10px 20px;
+		background: #8c8c8c;
+		box-shadow: 0 0 5px #ccc;
+		border-radius: 999px;
+		color: #fff;
+	}
+
+	.chat-block.user {
+		text-align: right;
+	}
+
+	.chat-block.user span {
+		background: #1890ff;
+		text-align: right;
 	}
 </style>
